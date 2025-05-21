b881ddf84c1d5b7bf1df6af68fd8469d4d0e99e1
Juan Esteban <102327436+JuanesVVV@users.noreply.github.com>
2025-05-20 20:43:43 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // **Validación de formulario**
-    document.getElementById("enviarBtn").addEventListener("click", function() {
-        const email = document.getElementById("email").value; // Captura el correo ingresado
-        const mensaje = document.getElementById("mensaje-error"); // Ubicación del mensaje de error
-        
-        // Expresión regular para validar correos electrónicos
-        const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        
-        if (!regexEmail.test(email)) {
-            mensaje.textContent = "Correo inválido, intenta nuevamente.";
-            mensaje.style.color = "red"; // Muestra el mensaje en rojo si el correo es incorrecto
-        } else {
-            mensaje.textContent = "Correo enviado correctamente.";
-            mensaje.style.color = "green"; // Muestra el mensaje en verde si el correo es válido
-        }
-    });
+
